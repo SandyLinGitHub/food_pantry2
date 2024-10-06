4751527cbd0edd43f0955c832bd22d5279b85ec7
Miya <xkang07@syr.edu>
2024-10-06 01:03:00 -0400

    recipe2

--- my-app/src/pages/recipe.js
+++ my-app/src/pages/recipe.js
@@ -1,16 +1,3 @@
-// // recipe.js
-// import React from 'react';
-
-// function RecipePage() {
-//   return (
-//     <div>
-//       <h1>recipe Page</h1>
-//     </div>
-//   );
-// }
-
-// export default RecipePage;
-
 import React from 'react';
 import './recipe.css';
 
